4e396c0943f9887004b9fbbed399ed2006133770
khangngonhapho <khangngonhapho@users.noreply.github.com>
2026-07-23 01:38:04 +0700

    bump: Userscript version header to 1.4

--- static/js/thienkhoi_list_scraper.user.js
+++ static/js/thienkhoi_list_scraper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Khang Ngô BDS - Thiên Khôi List Scraper
 // @namespace    http://tampermonkey.net/
-// @version      1.3
+// @version      1.4
 // @description  Cào nhanh thông tin nhà phố từ danh sách Thiên Khôi về local server Khang Ngô
 // @author       Antigravity
 // @match        https://*.thienkhoi.com/*
